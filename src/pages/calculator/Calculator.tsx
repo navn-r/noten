0d3383da79cbbb687abf4bd2d2ffa278f00cf2aa
Navinn Ravindaran <59669957+navn-r@users.noreply.github.com>
2021-01-29 17:48:16 -0500

    feat(calculator-page): modal done

--- src/pages/calculator/Calculator.tsx
+++ src/pages/calculator/Calculator.tsx
@@ -1,4 +1,17 @@
-import { IonAvatar, IonButton } from "@ionic/react";
+import {
+  IonAvatar,
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonInput,
+  IonModal,
+  IonRippleEffect,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { checkmarkCircle, closeCircle } from "ionicons/icons";
 import React, { useState } from "react";
 import { useAuth } from "../../auth/AuthContext";
 import PageWrapper from "../../components/page-wrapper/PageWrapper";
@@ -9,9 +22,9 @@ const ICON_URL = process.env.PUBLIC_URL + "/assets/icon/logo-circle.png";
 type DataValue = { [key in DataKey]: string | number };
 
 enum DataKey {
-  CURRENT = "current",
-  WEIGHT = "weight",
-  GOAL = "goal",
+  current = "current",
+  weight = "weight",
+  goal = "goal",
 }
 
 const CHAT_DIALOGS = {
@@ -31,41 +44,152 @@ const INITIAL_DATA = {
   current: 69,
   weight: 8.832,
   goal: 100,
-  showCalculation: false,
+};
+
+interface NumberModalProps {
+  showModal: boolean;
+  title: string;
+  value: number;
+  onDismiss: Function;
+  onSuccess: Function;
+}
+
+const NumberModal: React.FC<NumberModalProps> = ({
+  showModal,
+  title,
+  value: initialValue,
+  onDismiss,
+  onSuccess,
+}) => {
+  const [value, setValue] = useState(initialValue);
+  const [showSuccess, setShowSuccess] = useState(true);
+
+  const onChangeValue = (value: string): void => {
+    if (!value.length) return;
+    let newVal = parseInt(value, 10);
+    if (isNaN(newVal)) return;
+
+    setShowSuccess(newVal >= 0 && newVal <= 100);
+    setValue(newVal);
+  };
+
+  return (
+    <IonModal
+      isOpen={showModal}
+      mode="ios"
+      cssClass="number-modal"
+      swipeToClose={true}
+      onDidDismiss={() => {
+        setValue(initialValue);
+        onDismiss();
+      }}
+    >
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonButton
+              mode="md"
+              color="danger"
+              fill="clear"
+              shape="round"
+              onClick={() => {
+                setValue(initialValue);
+                onDismiss();
+              }}
+            >
+              <IonIcon slot="icon-only" icon={closeCircle} />
+            </IonButton>
+          </IonButtons>
+          <IonTitle>{title}</IonTitle>
+          <IonButtons slot="end">
+            <IonButton
+              mode="md"
+              color="success"
+              disabled={!showSuccess}
+              onClick={() => onSuccess(value)}
+            >
+              <IonIcon slot="icon-only" icon={checkmarkCircle} />
+            </IonButton>
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <div className="input-wrapper">
+          <IonInput
+            clearInput={true}
+            inputMode="decimal"
+            type="number"
+            value={value}
+            onIonChange={({ detail }) =>
+              detail.value && onChangeValue(detail.value)
+            }
+          />
+        </div>
+        <div className="info-text">
+          Enter any value between 0 and 100 (inclusive).
+        </div>
+      </IonContent>
+    </IonModal>
+  );
 };
 
 interface ChatBubbleProps {
   data: DataValue;
+  setData: any;
   index: DataKey;
   photoURL: string;
 }
 
-const ChatBubbleLine: React.FC<ChatBubbleProps> = ({
-  data,
-  index,
-  photoURL,
-}) => {
-  return (
-    <div className="line">
-      <div className="left">
-        <IonAvatar slot="start">
-          <img src={ICON_URL} alt="" />
-        </IonAvatar>
-        <div className="bubble">{CHAT_DIALOGS.messages[index]}</div>
-      </div>
-      <div className="right">
-        <IonAvatar slot="end">
-          <img src={photoURL} alt="" />
-        </IonAvatar>
-        <div className="bubble">{data[index]}%</div>
-      </div>
-    </div>
-  );
-};
+const ChatBubbleLine: React.FC<ChatBubbleProps> = React.memo(
+  ({ data, setData, index, photoURL }) => {
+    const [showModal, setShowModal] = useState(false);
+    const successHandler = (value: number): void => {
+      let dataCopy = data;
+      dataCopy[index] = value;
+      setData(dataCopy);
+      setShowModal(false);
+    };
 
-const Calculation: React.FC<any> = ({ data }) => {
-  // TODO: change any to props
-  const { goal, current, weight } = data;
+    return (
+      <>
+        <NumberModal
+          showModal={showModal}
+          title={CHAT_DIALOGS.prompts[index]}
+          value={data[index] as number}
+          onDismiss={() => setShowModal(false)}
+          onSuccess={successHandler}
+        />
+        <div className="line">
+          <div className="left">
+            <IonAvatar slot="start">
+              <img src={ICON_URL} alt="" />
+            </IonAvatar>
+            <div className="bubble">{CHAT_DIALOGS.messages[index]}</div>
+          </div>
+          <div className="right">
+            <IonAvatar slot="end">
+              <img src={photoURL} alt="" />
+            </IonAvatar>
+            <div
+              className="ion-activatable bubble"
+              onClick={() => setShowModal(true)}
+            >
+              {data[index]}%<IonRippleEffect></IonRippleEffect>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+);
+
+interface DataProps {
+  current: number;
+  weight: number;
+  goal: number;
+}
+
+const Calculation: React.FC<DataProps> = ({ goal, current, weight }) => {
   const grade = ((goal - current * (1 - weight / 100)) / weight) * 100;
   const colorClass =
     "grade-" + (grade >= 100 ? "danger" : grade <= 0 ? "success" : "warning");
@@ -74,55 +198,71 @@ const Calculation: React.FC<any> = ({ data }) => {
 
 const Calculator: React.FC = () => {
   const { user } = useAuth();
-  const [data, setData] = useState(INITIAL_DATA);
+  const [data, setData] = useState({ ...INITIAL_DATA });
+  const [showCalculation, setShowCalculation] = useState(false);
+
+  const resetHandler = () => {
+    setData({ ...INITIAL_DATA });
+    setShowCalculation(false);
+  };
 
   return (
     <PageWrapper>
-      <h5 id="title">Grade Predictor</h5>
-      <div className="messages">
-        {["current", "weight", "goal"].map((value, index) => (
-          <ChatBubbleLine
-            key={index}
-            data={data}
-            index={value as DataKey}
-            photoURL={user.photoURL}
-          />
-        ))}
-        {data.showCalculation && (
-          <div className="line">
-            <div className="left">
-              <IonAvatar slot="start">
-                <img src={ICON_URL} alt="" />
-              </IonAvatar>
-              <div className="bubble">
-                You will need to score at least&nbsp;
-                <Calculation data={data} />
+      <div className="outer-wrapper">
+        <div className="top-container">
+          <IonToolbar id="title" mode="md">
+            <IonTitle>Grade Predictor</IonTitle>
+            <IonTitle size="small">
+              Tap the green chat bubbles to configure.
+            </IonTitle>
+          </IonToolbar>
+          <div className="messages">
+            {Object.keys(DataKey).map((value, index) => (
+              <ChatBubbleLine
+                key={index}
+                data={data}
+                setData={setData}
+                index={value as DataKey}
+                photoURL={user.photoURL}
+              />
+            ))}
+            <div
+              className="line"
+              style={{ opacity: showCalculation ? "1" : "0" }}
+            >
+              <div className="left">
+                <IonAvatar slot="start">
+                  <img src={ICON_URL} alt="" />
+                </IonAvatar>
+                <div className="bubble">
+                  You will need to score at least&nbsp;
+                  <Calculation {...data} />
+                </div>
               </div>
             </div>
           </div>
-        )}
+        </div>
+        <div className="button-container">
+          <IonButton
+            onClick={() => setShowCalculation(true)}
+            fill="outline"
+            color="success"
+            mode="md"
+            shape="round"
+          >
+            Calculate
+          </IonButton>
+          <IonButton
+            onClick={resetHandler}
+            mode="md"
+            fill="outline"
+            color="danger"
+            shape="round"
+          >
+            Reset
+          </IonButton>
+        </div>
       </div>
-      <div className="button-container">
-        <IonButton
-          onClick={() => setData({ ...data, showCalculation: true })}
-          fill="outline"
-          color="success"
-          mode="md"
-          shape="round"
-        >
-          Calculate
-        </IonButton>
-        <IonButton
-          onClick={() => setData({ ...INITIAL_DATA })}
-          mode="md"
-          fill="outline"
-          color="danger"
-          shape="round"
-        >
-          Reset
-        </IonButton>
-      </div>
-      <div className="info-text">Tap the green chat bubbles to configure.</div>
     </PageWrapper>
   );
 };
